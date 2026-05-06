c6baf5f03e8fca6a00171a6d983f5dd570eb5ede
Maria <mattosmariaf@gmail.com>
2026-05-06 15:41:15 -0300

    configurações express

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
+import express from 'express';
 
+const port = 3000;
+const app = express();
+
+app.get('/movies', (req, res) => {
+    res.send('Listagem de filmes');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
