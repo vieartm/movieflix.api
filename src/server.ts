b7d172cc43ce69f6c7df4c47bcf8f1e51a4590bb
Maria <mattosmariaf@gmail.com>
2026-05-08 13:34:01 -0300

    adicionado ordernação, gênero e idioma do filme

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,28 @@
+import 'dotenv/config';
 import express from 'express';
+import { PrismaClient } from './generated/prisma/client.js';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 const port = 3000;
 const app = express();
 
-app.get('/movies', (req, res) => {
-    res.send('Listagem de filmes');
+const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL,
+});
+
+const prisma = new PrismaClient({ adapter });
+
+app.get('/movies', async (_, res) => {
+    const movies = await prisma.movie.findMany({
+        orderBy: {
+            title: 'asc',
+        },
+        include: {
+            genres: true,
+            languages: true,
+        }
+    });
+    res.json(movies);
 });
 
 app.listen(port, () => {
